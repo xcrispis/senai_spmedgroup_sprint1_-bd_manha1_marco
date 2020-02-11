23e9338a8a4ead1cbe38f9a3d6f31ea352803a24
FIC <FIC@info.local>
2020-02-11 11:24:03 -0200

    Scripts completos

---
+++ DDL.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE medgroup
+USE medgroup
+
+
+
+CREATE TABLE TipoUsuario (id_tipo INT IDENTITY PRIMARY KEY, NomeTipo varchar(100) not null)
+
+CREATE TABLE Usuario (id_usuario INT IDENTITY PRIMARY KEY, Nome varchar(255) not null,  RG nchar(9) not null unique, CPF nvarchar(11) not null unique,
+Endereco nvarchar(300) not null, DataNascimento DATE not null, Email nvarchar(300) not null unique, Senha nvarchar(300) not null,
+CONSTRAINT CK_CPF CHECK (CPF LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+CONSTRAINT CK_RG CHECK (RG LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'), id_tipo INT FOREIGN KEY REFERENCES TipoUsuario(id_tipo) not null)
+
+CREATE TABLE Paciente (id_paciente INT IDENTITY PRIMARY KEY, id_usuario INT FOREIGN KEY REFERENCES Usuario(id_usuario) not null)
+
+CREATE TABLE Medico (id_medico INT IDENTITY PRIMARY KEY, CRM varchar(7) not null, CONSTRAINT CK_CRM CHECK (CRM LIKE '[0-9][0-9][0-9][0-9][0-9][A-Z][A-Z]'),
+id_usuario INT FOREIGN KEY REFERENCES Usuario(id_usuario) not null)
+
+CREATE TABLE Especialidade (id_especialidade INT IDENTITY PRIMARY KEY, NomeEspecialidade nvarchar(100) not null)
+
+CREATE TABLE MedicoEspecialidade (id_med_espec INT IDENTITY PRIMARY KEY, id_medico INT FOREIGN KEY REFERENCES Medico(id_medico) not null,
+id_especialidade INT FOREIGN KEY REFERENCES Especialidade(id_especialidade) not null)
+
+CREATE TABLE Situacao (id_situacao INT IDENTITY PRIMARY KEY, NomeSituacao varchar(15) not null)
+
+CREATE TABLE Clinica (id_clinica INT IDENTITY PRIMARY KEY, NomeFantasia nvarchar(300), Localizacao nvarchar(300) not null, CNPJ varchar(14), RazaoSocial nvarchar(300) not null, 
+CONSTRAINT CK_CNPJ CHECK (CNPJ LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'))
+
+CREATE TABLE ValorConsulta (id_valor_consulta INT IDENTITY PRIMARY KEY, NomeConsulta varchar(200), Valor decimal NOT NULL)
+
+CREATE TABLE Consulta (id_consulta INT IDENTITY PRIMARY KEY, horario_consulta DATETIME NOT NULL, descricao varchar(1500) NOT NULL,id_medico INT FOREIGN KEY REFERENCES Medico(id_medico) NOT NULL,
+id_situacao INT FOREIGN KEY REFERENCES Situacao(id_situacao) NOT NULL, id_paciente INT FOREIGN KEY REFERENCES Paciente(id_paciente) NOT NULL, 
+id_valor_consulta INT FOREIGN KEY REFERENCES ValorConsulta(id_valor_consulta) NOT NULL, id_clinica INT FOREIGN KEY REFERENCES Clinica(id_clinica) NOT NULL)
---
+++ DML.sql
@@ -0,0 +1,33 @@
+INSERT INTO TipoUsuario(NomeTipo) VALUES ('Admin'), ('Paciente'), ('Medico')
+
+CREATE TRIGGER SeparaMedico
+ON Usuario
+AFTER INSERT
+AS
+CASE WHEN COLUNA = 2 THEN INSERT INTO Medico(id_usuario, CRM) VALUES (SELECT USUARIO.ID_USUARIO WHERE ) ELSE 
+BEGIN
+
+
+
+INSERT INTO Usuario(Nome, RG, CPF, Endereco, DataNascimento, Email, Senha, id_tipo) VALUES ('João', '123456789', '12345678910', 'Praça Jequitiba', '04/05/2000', 'oloco@gmail.com' ,'macaco123ouro', '1'), 
+('Kleber', '987647584', '84756475748', 'Paraça De Lá', '02/03/1987', 'ugabuga@gmail.com' ,'ParanáJaragua', 2), 
+('Maicão', '283746584', '28374672312', 'Lugar Algum', '03/09/1999', 'feijoada123' ,'jorginpega12@hotmail.com', 3 ), 
+('Jorge',  '873454523', '12312312312', 'Pra lá de bogoda', '02/09/1987', 'caminhao321', 'loucodosaco@gmail.com', 1), 
+('Everson','877872634', '12374678371', 'Rua joaquin mendes, 27', '03/02/2000', 'busca123', 'geffinho@gmail.com', 2) 
+
+INSERT INTO Paciente(id_usuario) VALUES ('2'), ('5')
+
+INSERT INTO Medico(id_usuario, CRM) VALUES ('3', '34545RG')
+
+INSERT INTO Especialidade(NomeEspecialidade) VALUES ('Virologista'), ('Bacteriologista'), ('Clinico Geral')
+
+INSERT INTO MedicoEspecialidade(id_medico, id_especialidade) VALUES (1, 1)
+
+INSERT INTO Situacao(NomeSituacao) VALUES ('Realizada'), ('Agendada'), ('Cancelada')
+
+INSERT INTO Clinica(NomeFantasia, Localizacao, CNPJ, RazaoSocial) VALUES ('Clinica Josias LTDA', 'Praça Albuquerque, 72', '29384736562123', 'Clinica Josias'), 
+('Clinica Matias LTDA', 'Logradouro Jorge, 183', '29384756783456', 'Clinica Matias') 
+
+INSERT INTO ValorConsulta(NomeConsulta, Valor) VALUES ('Ortopédica Infantil',  2000), ('Ortopédica Adulta', 3000)
+
+INSERT INTO Consulta(horario_consulta, descricao, id_medico, id_situacao, id_paciente, id_valor_consulta, id_clinica) VALUES ('17:00:00', 'Consulta Bacana', 1, 1, 2, 1, 1)
